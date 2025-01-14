f0c9f47589f586d8bfd33b63fe83b2e83a818296
Roberto Fierimonte <roberto.fierimonte@gmail.com>
2025-01-14 22:58:57 +0200

    Refactored repo to work on second assignment
    Signed-off-by: Roberto Fierimonte <roberto.fierimonte@gmail.com>

---
+++ assignment-2/src/model.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as I
+
+
+# define the CNN architecture
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+
+        super().__init__()
+        self.backbone = nn.Sequential(
+            # Block 1
+            nn.Conv2d(3, 64, 3, padding=1),
+            # nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, padding=1),
+            # nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            # Output size: N x 112 x 112 x 64
+
+            # Block 2
+            nn.Conv2d(64, 128, 3, padding=1),
+            # nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 3, padding=1),
+            # nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            # Output size: N x 56 x 56 x 128
+
+            # Block 3
+            nn.Conv2d(128, 256, 3, padding=1),
+            # nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            # nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            # nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            # Output size: N x 28 x 28 x 256
+
+            # Block 4
+            nn.Conv2d(256, 512, 3, padding=1),
+            # nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            # nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            # nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            # Output size: N x 14 x 14 x 512
+
+            # Block 5
+            nn.Conv2d(512, 512, 3, padding=1),
+            # nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            # nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            # nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            # Output size: N x 7 x 7 x 512
+        )
+        self.head = nn.Sequential(
+            nn.Linear(7 * 7 * 512, 4096, bias=True),
+            nn.BatchNorm1d(4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout1d(dropout, inplace=False),
+            nn.Linear(4096, 4096, bias=True),
+            nn.BatchNorm1d(4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout1d(dropout, inplace=False),
+            nn.Linear(4096, num_classes, bias=True),
+        )  # Output size: N x num_classes
+
+        # Add weights initialisation
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                I.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    I.constant_(m.bias, 0)
+            elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+                I.constant_(m.weight, 1)
+                I.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                I.xavier_normal_(m.weight)
+                I.constant_(m.bias, 0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.backbone(x)
+        x = torch.flatten(x, 1)
+        x = self.head(x)
+        return x
+
+
+######################################################################################
+#                                     TESTS
+######################################################################################
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_loaders():
+    from .data import get_data_loaders
+
+    return get_data_loaders(batch_size=2)
+
+
+def test_model_construction(data_loaders):
+
+    model = MyModel(num_classes=23, dropout=0.3)
+
+    dataiter = iter(data_loaders["train"])
+    images, labels = next(dataiter)
+
+    out = model(images)
+
+    assert isinstance(
+        out, torch.Tensor
+    ), "The output of the .forward method should be a Tensor of size ([batch_size], [n_classes])"
+
+    assert out.shape == torch.Size(
+        [2, 23]
+    ), f"Expected an output tensor of size (2, 23), got {out.shape}"
